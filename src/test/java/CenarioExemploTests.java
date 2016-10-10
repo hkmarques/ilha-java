2cb29d92ab2bbcbcad99a747210e25fb78d2adf4
Herick Oliveira <hkmarques@gmail.com>
2016-10-10 20:52:33 -0300

    Implementacão preliminar de testes do cenário de exemplo.

--- src/test/java/CenarioExemploTests.java
+++ src/test/java/CenarioExemploTests.java
@@ -1,4 +1,10 @@
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 /**
  * Testes unitários da CLI para os casos de exemplo inclusos na descrição
@@ -6,64 +12,77 @@ import org.junit.Test;
  */
 public class CenarioExemploTests {
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    private CLI cli;
+
+    @Before
+    public void setUp() {
+        cli = new CLI();
+    }
+
     @Test
     public void distanciaABC() {
-        
-
+        int obtida = cli.getDistancia(Rota.new("A", "B", "C"));
+        assertEquals(9, obtida);
     }
 
     @Test
     public void distanciaAD() {
-        
-
+        int obtida = cli.getDistancia(Rota.new("A", "D"));
+        assertEquals(5, obtida);
     }
 
     @Test
     public void distanciaADC() {
-        
-
+        int obtida = cli.getDistancia(Rota.new("A", "D", "C"));
+        assertEquals(13, obtida);
     }
 
     @Test
     public void distanciaAEBCD() {
-        
-
+        int obtida = cli.getDistancia(Rota.new("A", "E", "B", "C", "D"));
+        assertEquals(22, obtida);
     }
 
     @Test
     public void distanciaAED() {
-        
-
+        thrown.expect(RotaInexistenteException.class);
+        cli.getDistancia(Rota.new("A", "E", "D"));
     }
 
     @Test
     public void inicioFimCMax3Paradas() {
-        
-
+        CriterioRota criterio = CriterioRota.new().inicio("C").fim("C").maximoParadas(3);
+        int obtidas = cli.getNumeroDeRotas(criterio);
+        assertEquals(2, obtidas);
     }
 
     @Test
     public void inicioAFimC4Paradas() {
-        
-
+        CriterioRota criterio = CriterioRota.new().inicio("A").fim("C").paradasExatas(4);
+        int obtidas = cli.getNumeroDeRotas(criterio);
+        assertEquals(3, obtidas);
     }
 
     @Test
     public void distanciaMaisCurtaAC() {
-        
-
+        int obtida = cli.getDistanciaMaisCurta(Rota.new("A", "C"));
+        assertEquals(9, obtida);
     }
 
     @Test
     public void distanciaMaisCurtaBB() {
-        
-
+        int obtida = cli.getDistanciaMaisCurta(Rota.new("B", "B"));
+        assertEquals(9, obtida);
     }
 
     @Test
     public void numeroRotasCCdistanciaMenorQue30() {
-        
-
+        CriterioRota criterio = CriterioRota.new().inicio("C").fim("C").distanciaMenorQue(30);
+        int obtidas = cli.getNumeroDeRotas(criterio);
+        assertEquals(7, obtidas);
     }
     
 }
